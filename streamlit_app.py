8d0c4c2318077d277d66ca33e2b1e86d1efcdc0f
deepu-a-LD <124342260+deepu-a-LD@users.noreply.github.com>
2023-12-06 13:34:05 +0530

    Update streamlit_app.py
    Added the lines for the header and texts

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,6 @@
 import streamlit
 streamlit.title('First demo with streamlit was successfull')
+streamlit.header('Breakfast menu')
+streamlit.text('Idly vada sambar')
+streamlit.text('Dosa chatni')
+streamlt.text('Puri curry')
