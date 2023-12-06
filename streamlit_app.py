3d16c1c529160a27e3f12b7239fc39ebad04b5c9
deepu-a-LD <124342260+deepu-a-LD@users.noreply.github.com>
2023-12-06 13:47:52 +0530

    Added header line

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,6 +5,8 @@ streamlit.text('Idly vada sambar')
 streamlit.text('Dosa chatni')
 streamlit.text('Puri curry')
 
+streamlit.header('Build your own Fruit smoothie')
+
 import pandas
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 streamlit.dataframe(my_fruit_list)
